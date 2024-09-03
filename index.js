24fd011f31dfe03828f8e0fa519a8806294ffea0
Fabricio Borgobello <fborgobello@gmail.com>
2024-09-03 12:56:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-// List of .env file names to merge (excluding the base .env)
 const envFiles = ['.env.development', '.env.production'];
 
-// Function to log with color
 function logWithColor(message, color) {
   console.log(`\x1b[${color}m%s\x1b[0m`, message);
 }
 
-// Function to merge .env files
 function mergeEnvFiles(baseEnvFile, additionalFiles) {
   let combinedEnv = {};
   let filesToMerge = [baseEnvFile, ...additionalFiles];
@@ -25,7 +22,7 @@ function mergeEnvFiles(baseEnvFile, additionalFiles) {
             logWithColor(
               `Warning: Duplicate key "${key}" found in ${file}. Using the latest value.`,
               33,
-            ); // Yellow
+            );
           }
           combinedEnv[key] = value;
         }
@@ -40,15 +37,12 @@ function mergeEnvFiles(baseEnvFile, additionalFiles) {
     .join('\n');
 }
 
-// Start message
-logWithColor('⏳ Creating .env file...', 36); // Cyan
+logWithColor('⏳ Creating .env file...', 36);
 
 // Check for an existing .env file
 const baseEnvFile = '.env';
 const mergedContent = mergeEnvFiles(baseEnvFile, envFiles);
 
-// Write the merged content to the .env file
 fs.writeFileSync(path.join(__dirname, baseEnvFile), mergedContent);
 
-// Success message
 logWithColor('✅ Merged .env file created successfully.', 32);
